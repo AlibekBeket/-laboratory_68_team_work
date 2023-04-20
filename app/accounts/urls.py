3b8a0c91e38bfcd3d14821cc426f515c2bb517f4
YerbolZhulniyazov <zhulniyazov@gmail.com>
2023-04-20 10:17:52 +0600

    #4 Прописаны url пути

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
 
+from accounts.views import RegisterView, LoginView, logout_view, UserChangeView
+
 urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('profile/<int:pk>/change/', UserChangeView.as_view(), name='change'),
 ]
