9f4f3791ba42cac1586b1b5e30fa670e80790303
YerbolZhulniyazov <zhulniyazov@gmail.com>
2023-04-20 07:53:06 +0600

    #3 Добавлена модели Резюме, Опыт работы и Образование
    Добавлена модель Resume, Experience и Education, заполнены необходимые поля, выполнена миграция. Модели были зарегистрированы в админке. Добавлено пробное резюме, опыт работы и образование через админку. Обновлены фикстуры

--- app/headhunter/admin.py
+++ app/headhunter/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
+
+from headhunter.models.resume import Resume, CategoryEducationChoices, Education, Experience
 from headhunter.models.vacancy import Vacancy, VacancyCategory
 
 
 class VacancyAdmin(admin.ModelAdmin):
     list_display = ('author', 'name', 'salary', 'description', 'experience_time', 'is_active', 'created_at',
-                    'updated_at', 'vacancy_category', 'is_deleted')
+                    'updated_at', 'vacancy_category', 'is_deleted', 'deleted_at')
     list_filter = ('author', 'name', 'salary', 'is_active', 'created_at', 'updated_at', 'vacancy_category')
     search_fields = ('author', 'name', 'salary', 'is_active', 'created_at', 'updated_at', 'vacancy_category')
     fields = ('author', 'name', 'salary', 'description', 'experience_time', 'is_active', 'vacancy_category')
@@ -13,3 +15,39 @@ class VacancyAdmin(admin.ModelAdmin):
 
 admin.site.register(VacancyCategory)
 admin.site.register(Vacancy, VacancyAdmin)
+
+
+class ResumeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'salary', 'info', 'email', 'phone', 'telegram', 'linkedin', 'facebook',
+                    'is_published', 'created_at', 'updated_at', 'is_deleted', 'deleted_at')
+    list_filter = ('name', 'author', 'salary', 'info', 'email', 'phone', 'telegram', 'linkedin', 'facebook',
+                   'is_published', 'created_at', 'updated_at')
+    search_fields = ('name', 'author', 'info', 'email', 'phone', 'telegram', 'linkedin', 'facebook',
+                     'is_published', 'created_at', 'updated_at')
+    fields = ('name', 'author', 'info', 'email', 'phone', 'telegram', 'linkedin', 'facebook', 'is_published')
+    readonly_fields = ['id']
+
+
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ('resume', 'institution_name', 'speciality', 'category_education', 'start_date', 'finish_date',
+                    'faculty', 'created_at', 'updated_at')
+    list_filter = ('resume', 'institution_name', 'speciality', 'category_education', 'start_date', 'finish_date',
+                   'faculty', 'created_at', 'updated_at')
+    search_fields = ('resume', 'institution_name', 'speciality', 'category_education', 'start_date', 'finish_date',
+                     'faculty')
+    fields = ('resume', 'institution_name', 'speciality', 'category_education', 'start_date', 'finish_date',
+              'faculty')
+    readonly_fields = ['id']
+
+
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ('resume', 'company', 'job_title', 'start_date', 'end_date', 'created_at', 'updated_at')
+    list_filter = ('resume', 'company', 'job_title', 'start_date', 'end_date')
+    search_fields = ('resume', 'company', 'job_title', 'start_date', 'end_date')
+    fields = ('resume', 'company', 'job_title', 'start_date', 'end_date')
+    readonly_fields = ['id']
+
+
+admin.site.register(Resume, ResumeAdmin)
+admin.site.register(Education, EducationAdmin)
+admin.site.register(Experience, ExperienceAdmin)
