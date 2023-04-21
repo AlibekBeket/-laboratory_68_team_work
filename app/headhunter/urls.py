15f4f35d1d2d9ceebb2158eaecab08fb1a9ab005
Alibek Beket <alibek1beket@gmail.com>
2023-04-22 00:05:34 +0600

    #5 url путь
    Создал путь до главной страницы

--- app/headhunter/urls.py
+++ app/headhunter/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 
+from headhunter.views.headhunter import VacancyListView
+
 urlpatterns = [
+    path('', VacancyListView.as_view()),
+    path('headhunter/', VacancyListView.as_view(), name='home_page'),
 ]
